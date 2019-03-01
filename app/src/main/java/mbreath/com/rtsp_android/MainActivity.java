b2d97d57c32198dfc30334413559f6de9132be62
Surya Jeet Singh <suryapps.in@gmail.com>
2019-03-01 16:05:57 +0530

    Just logging the accelerometer data

--- app/src/main/java/mbreath/com/rtsp_android/MainActivity.java
+++ app/src/main/java/mbreath/com/rtsp_android/MainActivity.java
@@ -1,13 +1,50 @@
 package mbreath.com.rtsp_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+    }
+
+    boolean capturingData = true;
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (capturingData && sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x, y, z;
+            x = sensorEvent.values[0];
+            y = sensorEvent.values[1];
+            z = sensorEvent.values[2];
+
+            Log.v("Accelerometer", String.format("x=%f y=%f z=%f", x, y, z));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
